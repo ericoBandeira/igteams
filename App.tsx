f6a429beae6ba95d4d40a393a0c99640b9b939c6
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-09-23 16:01:33 -0300

    finish header

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 import { Groups } from "./src/screens/Groups";
+import { Loading } from "@components/Loading";
+
 import { ThemeProvider } from "styled-components/native";
 import theme from "@theme/index";
+import { ActivityIndicator, StatusBar } from "react-native";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
