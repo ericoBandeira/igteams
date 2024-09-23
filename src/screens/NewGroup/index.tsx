0a48e76e6f67bef6d34b0fc07fa5efac1ad29e0d
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-09-23 20:45:42 -0300

    Add asyncStorage and finish app

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,22 +1,44 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+
+import { AppError } from "@utils/AppError";
+import { groupCreate } from "@storage/group/groupCreate";
+
 import { Container, Content, Icon } from "./styles";
+
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
 
 export function NewGroup() {
-  const navigation = useNavigation();
   const [group, setGroup] = useState("");
 
-  function handleNew() {
-    navigation.navigate("players", { group });
+  const navigation = useNavigation();
+
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
+    }
   }
 
   return (
     <Container>
       <Header showBackButton />
+
       <Content>
         <Icon />
 
